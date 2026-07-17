2db73b6f9124e5857e5b29673e91b0e1f9802e05
Hemant Sharma <hemantsharmafli7@gmail.com>
2026-07-18 03:02:34 +0530

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Plus_Jakarta_Sans } from "next/font/google"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={plusJakarta.className}>{children}</body>
+      <body className={plusJakarta.className}>
+        {children}
+        <Analytics />
+        </body>
     </html>
   )
 }
